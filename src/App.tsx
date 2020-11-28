67b08689455547d368542ff4336488b82d9184a9
masham04 <66207787+masham04@users.noreply.github.com>
2020-11-28 19:28:57 +0500

    main update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu'
-// import './App.css';
-// import logo from './logo.png'
-// import {Home} from './Home'
+import './App.css';
+import logo from './logo.png'
+import {Home} from './Home'
 
 
 
 
  function App() {
-  console.log('App running')
-   return (
-    <Menu>
-    <a id="home" className="menu-item" href="/">Home</a>
-    <a id="about" className="menu-item" href="/about">About</a>
-    <a id="contact" className="menu-item" href="/contact">Contact</a>
-    
-  </Menu>
-//     <>
-//       <div className='top'>
-//        <img src={logo} width='300px' height='80px'/>
-//       </div>
-//       <Home /> 
+  return(
+    <>
+      <div className='top'>
+       <img src={logo} width='300px' alt='logo' height='80px'/>
+      </div>
+      <Home /> 
       
-//     </>
+    </>
   );
  }
 
